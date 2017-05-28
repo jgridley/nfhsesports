a9f1dc31be62b631cb6a2cc3af8bc4305456a3fa
Joshua Gridley <jgrid25@gmail.com>
2017-05-27 21:11:39 -0500

    More stuff that I'm too lazy to type about. Just read the changes xd

---
+++ js/coming-soon.js
@@ -0,0 +1,20 @@
+var countDownDate = new Date("Jan 1, 2018 15:37:25").getTime();
+
+var countdownfunction = setInterval(function() {
+
+    var now = new Date().getTime();
+    
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
