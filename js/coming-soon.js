cdb0b3aec16c4f579006175de701dbe281520d2a
Joshua Gridley <jgrid25@gmail.com>
2017-05-31 13:53:11 -0500

    Made the countdown timer more efficient and easier to read.

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,20 +1,22 @@
 var countDownDate = new Date("Jan 1, 2018 15:37:25").getTime();
 
 var countdownfunction = setInterval(function() {
-
     var now = new Date().getTime();
-    
     var distance = countDownDate - now;
-    
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    
+
     if (distance < 0) {
         clearInterval(countdownfunction);
         document.getElementById("demo").innerHTML = "EXPIRED";
+        return;
     }
+
+    var seconds = Math.floor(distance / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var seconds = seconds % 60;
+    var hours = Math.floor(minutes / 60);
+    var minutes = minutes % 60;
+    var days = Math.floor(hours / 24);
+    var hours = hours % 24;
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 }, 1000);
